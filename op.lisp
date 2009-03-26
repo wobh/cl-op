0b44595ca5187efc29b9d0c46bb6add618631c3e
simon.belak <simon.belak@345274fc-069f-11de-9036-69e38a880166>
2009-03-26 20:55:43 +0000

    coscosmetics

--- op.lisp
+++ op.lisp
@@ -1,12 +1,14 @@
 (in-package :cl-op)
 
 (defun disjoin (&rest predicates)
-  "Combine results of PREDICATES by OR."
-  (lambda (&rest args) (some (lambda (p) (apply p args)) predicates)))
-  
+  "OR predicate combinator."
+  (lambda (&rest arguments) 
+    (some (lambda (predicate) (apply predicate arguments)) predicates)))
+    
 (defun conjoin (&rest predicates)
-  "Combine results of PREDICATES by AND."
-  (lambda (&rest args) (every (lambda (p) (apply p args)) predicates)))
+  "AND predicate combinator."
+  (lambda (&rest arguments) 
+    (every (lambda (predicate) (apply predicate arguments)) predicates)))
   
 (defun starts-with (list head)
   "Does LIST start with HEAD?"
@@ -24,34 +26,35 @@
            (some (lambda (head) (starts-with form head)) '(quote op op*))
            (and (starts-with form 'function) (symbolp (second form))))))
       
-(defun walk (fn form)
-  "Walk FORM applying FN to each node."
-  (multiple-value-bind (form end-walk-p) (funcall fn form)
+(defun walk (function form)
+  "Walk FORM applying FUNCTION to each node."
+  (multiple-value-bind (form end-walk-p) (funcall function form)
     (cond (end-walk-p form)
-          ((recurp form) (mapcar (lambda (form) (walk fn form)) form))
+          ((recurp form) (mapcar (lambda (form) (walk function form)) form))
           (t form))))
             
-(defun simple-slot-p (obj)
-  "Is OBJ a simple slot designator?"
-  (eq obj (intern "_")))
+(defun simple-slot-p (object)
+  "Is OBJECT a simple slot designator?"
+  (eq object (intern "_")))
 
-(defun rest-slot-p (obj)
-  "Is OBJ a rest slot designator?"
-  (eq obj (intern "__")))
+(defun rest-slot-p (object)
+  "Is OBJECT a rest slot designator?"
+  (eq object (intern "__")))
   
-(defun slotp (obj)
-  "Is OBJ a slot designator?"
-  (or (simple-slot-p obj) (rest-slot-p obj)))
+(defun slotp (object)
+  "Is OBJECT a slot designator?"
+  (or (simple-slot-p object) (rest-slot-p object)))
                       
-(defun collect-slots (form)
+(defun slots-to-arguments (form)
   "Assign names to slots."
   (let ((slots))
-    (flet ((collect (arg)
-             (cond ((slotp arg)
-                    (when (rest-slot-p arg) (push '&rest slots))
-                    (first (push (gensym "OP-") slots)))
-                   (t arg))))
-      (values (walk #'collect form) (reverse slots)))))
+    (values (walk (lambda (node)
+                    (cond ((slotp node)
+                           (when (rest-slot-p node) (push '&rest slots))
+                           (first (push (gensym "OP-") slots)))
+                          (t node))) 
+                  form)
+            (reverse slots))))
 
 (defun liftablep (form)
   "Is FORM suitable for early evaluation?"
@@ -62,40 +65,42 @@
   (and (consp form) 
        (funcall (disjoin #'special-operator-p #'macro-function) (first form))))
 
-(defmacro with-rebinder (collector-fn &body body)
-  "Locally define collector function for bindings named COLLECTOR-FN."
-  (let ((bindings (gensym)))
-    `(let ((,bindings ,(second collector-fn)))
-       (flet ((,(first collector-fn) (&optional obj) 
-                (if obj
-                    (first (or (find obj ,bindings :test #'equal :key #'second)
-                               (first (push (list (gensym) obj) ,bindings))))
-                    (reverse ,bindings))))
+(defmacro with-rebinder (collector &body body)
+  "Locally define collector function for bindings named COLLECTOR."
+  (let ((binds (gensym)))
+    `(let ((,binds ,(second collector)))
+       (flet ((,(first collector) (&optional datum) 
+                (if datum
+                    (first (or (find datum ,binds :test #'equal :key #'second)
+                               (first (push (list (gensym) datum) ,binds))))
+                    (reverse ,binds))))
          ,@body))))
 
-(defun collect-invariants (form &key env)
+(defun lift-invariants (form &key environment)
   "Bind subforms suitable for early evaluation."
   (with-rebinder (bind)
-    (flet ((collect (arg)             
-             (if (liftablep (macroexpand arg env)) 
-                 (bind arg) 
-                 (values arg (specialp arg)))))
-      (values (walk #'collect form) (bind)))))
+    (values (walk (lambda (node)
+                    (if (liftablep (macroexpand node environment)) 
+                        (bind node) 
+                        (values node (specialp node)))) 
+                  form) 
+            (bind))))
 
 (defun with-bindings (bindings form)
   "Lexically enclose FORM in BINDINGS."
   (if bindings `(let ,bindings ,form) form))
-      
-(defmacro op* (&rest args)
+
+(defmacro op* (&rest form)
   "Make an anonymous function with implicit arguments. Defer evaluation."
-  (multiple-value-bind (form slots) (collect-slots args)
+  (multiple-value-bind (form slots) (slots-to-arguments form)
     `(lambda ,slots ,form)))
             
-(defmacro op (&rest args &environment env)
+(defmacro op (&rest form &environment environment)
   "Make an anonymous function with implicit arguments."
-  (multiple-value-bind (args invariants) (collect-invariants args :env env)
-    (with-bindings invariants `(op* ,@args))))
+  (multiple-value-bind (form invariants) 
+                       (lift-invariants form :environment environment)
+    (with-bindings invariants `(op* ,@form))))
    
-(defun flip (fn)
-  "Switch the first two arguments of FN."
-  (lambda (x y &rest args) (apply fn y x args)))
+(defun flip (function)
+  "Switch the first two arguments of FUNCTION."
+  (lambda (x y &rest arguments) (apply function y x arguments)))
